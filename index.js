45601fb546f7c2654f26190d2c94ec1a3f2fcbd1
crypticsurfer <cb.connor.bishop@gmail.com>
2023-11-25 23:10:44 -0700

    finsihed switch statement in index.js

--- index.js
+++ index.js
@@ -1,126 +1,138 @@
 // Include packages needed for this application
 var inquirer = require('inquirer');
 const Data = require('./lib/data');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const data = new Data();
 
-// Create an array of questions for user input
-const questions = [
-        {
-          type: 'list',
-          name: 'option',
-          message: "What would you like to do?",
-          choices: ["View all departments", "View all roles", "View all employees", "Add a department", 
-        "Add a role", "Add an employee", "Update an employee's role"]
-        },
-        {
-            type: 'text',
-            name: 'department-name',
-            message: "What is the department's name?",
-            when: (answers) => answers['option'] === 'Add a department',
-          },
-          {
-            type: 'text',
-            name: 'role-title',
-            message: "What is the role's title?",
-            when: (answers) => answers['option'] === 'Add a role',
-          },
-          {
-            type: 'text',
-            name: 'role-salary',
-            message: "What is the role's salary?",
-            when: (answers) => answers['option'] === 'Add a role',
-          },
-          {
-            type: 'list',
-            name: 'role-department',
-            message: "What department is the role in?",
-            choices: data.getDepartmentNames(),
-            when: (answers) => answers['option'] === 'Add a role',
-          },
-          {
-            type: 'text',
-            name: 'employee-first-name',
-            message: "What is the employee's first name?",
-            when: (answers) => answers['option'] === 'Add an employee',
-          },
-          {
-            type: 'text',
-            name: 'employee-last-name',
-            message: "What is the employee's last name?",
-            when: (answers) => answers['option'] === 'Add an employee',
-          },
-          {
-            type: 'list',
-            name: 'employee-role',
-            message: "What is the employee's role?",
-            choices: data.getRoleNames(),
-            when: (answers) => answers['option'] === 'Add an employee',
-          },
-          {
-            type: 'list',
-            name: 'employee-role',
-            message: "Who is the employee's manager?",
-            choices: data.getEmployeeNames(),
-            when: (answers) => answers['option'] === 'Add an employee',
-          },
-          {
-            type: 'list',
-            name: 'employee-role',
-            message: "Which employee needs to have their role updated?",
-            choices: data.getEmployeeNames(),
-            when: (answers) => answers['option'] === "Update an employee's role",
-          },
-          {
-            type: 'list',
-            name: 'employee-role',
-            message: "What is the employee's new role?",
-            choices: data.getRoleNames(),
-            when: (answers) => answers['option'] === "Update an employee's role",
-          },
-    ];
-
-
-// TODO: Create a function to creat SVG logo
-function createLogo(fileName, data) {
-    const filePath = path.join(fileName)
-    fs.writeFile(filePath, generateLogo(data), (err) =>
-    err ? console.log(err) : console.log(`Logo generated! @:${filePath}`)
-    );
-}
-
 // Create a function to initialize app
-function init() {
+async function init() {
+
   // create the connection to database
-  const connection = mysql.createConnection({
+  const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Maverick1',
     database: 'data'
   });
 
-  inquirer.prompt(questions).then((answers) => {
-      
-  })
-  .catch((error) => {
-      if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-      } else {
-      // Something else went wrong
-      }
-  });
+  // Create an array of questions for user input
+  const questions = [
+    {
+      type: 'list',
+      name: 'option',
+      message: "What would you like to do?",
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", 
+    "Add a role", "Add an employee", "Update an employee's role"]
+    },
+    {
+      type: 'text',
+      name: 'department-name',
+      message: "What is the department's name?",
+      when: (answers) => answers['option'] === 'Add a department',
+    },
+    {
+      type: 'text',
+      name: 'role-title',
+      message: "What is the role's title?",
+      when: (answers) => answers['option'] === 'Add a role',
+    },
+    {
+      type: 'text',
+      name: 'role-salary',
+      message: "What is the role's salary?",
+      when: (answers) => answers['option'] === 'Add a role',
+    },
+    {
+      type: 'list',
+      name: 'role-department',
+      message: "What department is the role in?",
+      choices: await data.getDepartmentChoices(connection),
+      when: (answers) => answers['option'] === 'Add a role',
+    },
+    {
+      type: 'text',
+      name: 'employee-first-name',
+      message: "What is the employee's first name?",
+      when: (answers) => answers['option'] === 'Add an employee',
+    },
+    {
+      type: 'text',
+      name: 'employee-last-name',
+      message: "What is the employee's last name?",
+      when: (answers) => answers['option'] === 'Add an employee',
+    },
+    {
+      type: 'list',
+      name: 'employee-role',
+      message: "What is the employee's role?",
+      choices: await data.getRoleChoices(connection),
+      when: (answers) => answers['option'] === 'Add an employee',
+    },
+    {
+      type: 'list',
+      name: 'employee-manager',
+      message: "Who is the employee's manager?",
+      choices: await data.getEmployeeChoices(connection),
+      when: (answers) => answers['option'] === 'Add an employee',
+    },
+    {
+      type: 'list',
+      name: 'employee-to-update',
+      message: "Which employee needs to have their role updated?",
+      choices: await data.getEmployeeChoices(connection),
+      when: (answers) => answers['option'] === "Update an employee's role",
+    },
+    {
+      type: 'list',
+      name: 'employee-new-role',
+      message: "What is the employee's new role?",
+      choices: await data.getRoleChoices(connection),
+      when: (answers) => answers['option'] === "Update an employee's role",
+    },
+  ];
 
+  inquirer.prompt(questions).then(async (answers) => { 
+    switch (answers['option']) {
+        case "View all departments":
+          rows = await data.getTableObject(connection, "department");
+          console.log("Departments:");
+          console.log("id \t name");
+          rows.forEach((row) => console.log(row['id'] + " \t " + row['name']));
+          break;
   
-  connection.end((error) => {
-    if (error) {
-      console.error('Error closing MySQL connection:', error);
-      return;
-    }
+        case "View all roles":
+          rows = await data.getTableObject(connection, "role");
+          console.log("Roles:");
+          console.log("id \t title \t salary \t department_id");
+          rows.forEach((row) => console.log(row['id'] + " \t " + row['title'] + " \t " + row['salary'] + " \t " + row['department_id']));
+          break;
+  
+        case "View all employees":
+          rows = await data.getTableObject(connection, "employee");
+          console.log("Employees:");
+          console.log("id \t first_name \t last_name \t role_id \t manager_id");
+          rows.forEach((row) => console.log(row['id'] + " \t " + row['first_name'] + " \t " + row['last_name'] + " \t " + row['role_id'] + " \t " + row['manager_id']));
+          break;
+  
+        case "Add a department":
+          await data.addDepartment(connection, answers['department-name']);
+          break;
+        
+        default:
+          console.log('No option selected.')
+      }
 
-    console.log('MySQL connection closed.');
+    // Close the MySQL connection
+    connection.end((error) => {
+      if (error) {
+        console.error('Error closing MySQL connection:', error);
+        return;
+      }
+    });
   });
 }
+
 
 // Function call to initialize app
 init();
